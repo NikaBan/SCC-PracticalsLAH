773ec7da0d8aeac303f3fd3a709c668f755d1260
NikaBan <nicpan977@gmail.com>
2018-07-03 12:37:04 +0800

    Am confused

---
+++ Ehh/Ehh/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ehh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Yeah, just give me some words?");
+            String words = Console.ReadLine();
+            Console.WriteLine("And here are your words again. With a lil extra spice");
+            Console.WriteLine("=> "+words+". Fuck life!");
+            
+        }
+    }
+}
