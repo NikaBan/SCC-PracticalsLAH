e25153bd0c7d3847833f7cbbc303c015fb4c942b
NikaBan <nicpan977@gmail.com>
2018-07-10 11:20:59 +0800

    added nana

---
+++ nana/nana/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This above all: to thine own self be true -- Shakespeare");
+        }
+    }
+}
